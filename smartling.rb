1966ed0324fe91d87cdecc7c8f3a1d4fca72f566
Marat Dreizin <mdreizin@fitbit.com>
2017-01-14 11:45:32 +0300

    chore(smartling): bumps version

--- smartling.rb
+++ smartling.rb
@@ -1,20 +1,16 @@
 class Smartling < Formula
   desc "CLI to upload and download translations"
-  homepage "https://github.com/fitbit/smartling"
-  url "https://github.com/Fitbit/smartling/archive/v0.4.4.tar.gz"
-  sha256 "f49340e807561fabc153a156a3e059717941b46e0c4c364273831c0804e28e43"
-
-  head "https://github.com/fitbit/smartling.git"
-
+  homepage "https://github.com/Fitbit/smartling"
+  url "https://github.com/Fitbit/smartling/archive/v0.4.5.tar.gz"
+  sha256 "ad0949d74e7f7a437a9496719c3d5dd92f8bfab52542dbff4b5788eb7302109c"
+  head "https://github.com/Fitbit/smartling.git"
   depends_on "go"
   depends_on "glide"
-
   def install
     ENV["GOPATH"] = buildpath
     glidepath = buildpath/".glide"
-    smartlingpath = buildpath/"src/github.com/fitbit/smartling"
+    smartlingpath = buildpath/"src/github.com/Fitbit/smartling"
     smartlingpath.install buildpath.children
-
     cd smartlingpath do
       system "glide", "--home", "#{glidepath}", "install", "--skip-test"
       system "go", "build", "-o", "smartling", "./cli/..."
@@ -22,7 +18,6 @@ class Smartling < Formula
       prefix.install_metafiles
     end
   end
-
   test do
     version = pipe_output("#{bin}/smartling --version")
     assert_match version.to_s, version
