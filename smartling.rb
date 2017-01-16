c709385d0a36544e45c322fde685d1b04c0ad342
Marat Dreizin <mdreizin@fitbit.com>
2017-01-16 23:42:25 +0300

    chore(smartling): bumps version

--- smartling.rb
+++ smartling.rb
@@ -1,8 +1,8 @@
 class Smartling < Formula
   desc "CLI to upload and download translations"
   homepage "https://github.com/Fitbit/smartling"
-  url "https://github.com/Fitbit/smartling/archive/v0.4.5.tar.gz"
-  sha256 "ad0949d74e7f7a437a9496719c3d5dd92f8bfab52542dbff4b5788eb7302109c"
+  url "https://github.com/Fitbit/smartling/archive/v0.5.0.tar.gz"
+  sha256 "54d6039af86a9a2651bf7b193097584f68c9dd93b26184caf9da014bc899e562"
   head "https://github.com/Fitbit/smartling.git"
   depends_on "go"
   depends_on "glide"
